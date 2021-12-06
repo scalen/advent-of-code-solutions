e551992b5fdebf23d4cffac748542d45bb7cadb9
David Monks <david.monks@octoenergy.com>
2021-12-06 11:12:46 +0000

    Start the repo with a template for future Go solutions

---
+++ templates/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	parts := []func(<-chan string, chan<- string) {
+	}
+
+	instructionsChannels := make([]chan string, len(parts))
+	for i := range instructionsChannels {
+		instructionsChannels[i] = make(chan string, 100)
+	}
+	solutionChannels := make([]chan string, len(parts))
+	for i := range instructionsChannels {
+		solutionChannels[i] = make(chan string)
+	}
+	for i, part := range parts {
+		go part(instructionsChannels[i], solutionChannels[i])
+	}
+
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		instruction := scanner.Text()
+		for _, instructions := range instructionsChannels {
+			instructions <- instruction
+		}
+	}
+	for _, instructions := range instructionsChannels {
+		close(instructions)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	for _, solutions := range solutionChannels {
+		for solution := range solutions {
+			fmt.Printf("%s\n", solution)
+		}
+	}
+}
