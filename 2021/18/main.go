47f3fdff299da56b8a40535282a857a3fb0fb7ff
David Monks <david.monks@octoenergy.com>
2021-12-20 12:51:24 +0000

    Initialise for days 18 and 19
    These days were missed due to other commitments in the weekend before
    Christmas!

---
+++ 2021/18/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func partTwo(input <-chan string, solutions chan<- string) {
+	defer close(solutions)
+
+	for range input {}
+
+	solutions <- fmt.Sprintf("Part 2")
+}
+
+func partOne(input <-chan string, solutions chan<- string) {
+	defer close(solutions)
+
+	for range input {}
+
+	solutions <- fmt.Sprintf("Part 1")
+}
+
+func main() {
+	parts := []func(<-chan string, chan<- string) {
+		partOne,
+		partTwo,
+	}
+
+	instructionsChannels := make([]chan string, len(parts))
+	for i := range instructionsChannels {
+		instructionsChannels[i] = make(chan string, 100)
+	}
+	solutionChannels := make([]chan string, len(parts))
+	for i := range instructionsChannels {
+		solutionChannels[i] = make(chan string)
+	}
+	for i, part := range parts {
+		go part(instructionsChannels[i], solutionChannels[i])
+	}
+
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		instruction := scanner.Text()
+		for _, instructions := range instructionsChannels {
+			instructions <- instruction
+		}
+	}
+	for _, instructions := range instructionsChannels {
+		close(instructions)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	for _, solutions := range solutionChannels {
+		for solution := range solutions {
+			fmt.Printf("%s\n", solution)
+		}
+	}
+}
