889ad6f94a572a4197be3330a69900ce90b78a87
David Monks <david.monks@tradenation.com>
2023-12-01 12:07:46 +0000

    feat(Haskell): Improve template with default solution definitions
    Prior to this change, one `solve` function had to handle the
    specification of a part to solve, which spread stuff out too much.
    
    This commit handles identification of the part to solve (or solving of
    both parts in one run) in `main`, and calls one (or both) of `solve1` or
    `solve2`, depending on the part specified on the CLI.

--- templates/main.hs
+++ templates/main.hs
@@ -1,15 +1,22 @@
 import System.Environment
 
-solve :: Char -> String -> ...
-solve part = case part of
-               '1' -> ...
-               '2' -> ...
+solve1 :: String -> ...
+solve2 :: String -> ...
 
 main :: IO ()
 main = do
   args <- getArgs
   case args of
-    [[part], inputFilepath] -> do
-                         input <- readFile inputFilepath
-                         print $ solve part input
-    _               -> putStrLn "Wrong number of arguments\n\nUsage:\n  main INPUT_FILEPATH PART_TO_SOLVE"
+    ['1', inputFilepath] -> do
+                              input <- readFile inputFilepath
+                              print $ solve1 input
+    ['2', inputFilepath] -> do
+                              input <- readFile inputFilepath
+                              print $ solve2 input
+    [inputFilepath]      -> do
+                              input <- readFile inputFilepath
+                              putStr "Part 1: "
+                              print $ solve1 input
+                              putStr "Part 2: "
+                              print $ solve2 input
+    _                    -> putStrLn "Wrong number of arguments\n\nUsage:\n  main [ 1 | 2 ] INPUT_FILEPATH"
