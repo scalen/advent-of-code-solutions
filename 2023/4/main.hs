2661af9ac0cb85638742890d9332541220057300
David Monks <david.monks@tradenation.com>
2023-12-05 13:56:05 +0000

    feat(2023): Day 4, Part 1

--- 2023/4/main.hs
+++ 2023/4/main.hs
@@ -1,11 +1,56 @@
 import System.Environment
 
+import qualified Data.Bits as Bits
+
+swallowSpace :: String -> String
+swallowSpace (' ':cs) = swallowSpace cs
+swallowSpace cs       = cs
+
+parseToken :: (Char -> Bool) -> String -> (String, String)
+parseToken _ "" = ("", "")
+parseToken condition (c:cs)
+  | condition c = (c:token, remainder)
+  | otherwise   = ("", swallowSpace (c:cs))
+  where
+    (token, remainder) = parseToken condition cs
+
+parseNumber :: String -> (Int, String)
+parseNumber line = (read numberStr, remainder)
+  where
+    (numberStr, remainder) = parseToken (\c -> '0' <= c && c <= '9') $ swallowSpace line
+
+parseNumberList :: String -> ([Int], String)
+parseNumberList ""       = ([], "")
+parseNumberList ('|':cs) = ([], cs)
+parseNumberList line     = (number:numbers, lineRemainder)
+  where
+    (number, listRemainder) = parseNumber line
+    (numbers, lineRemainder) = parseNumberList listRemainder
+
+data Card = Card Int [Int] [Int]
+
+parseCard :: String -> Card
+parseCard ('C':'a':'r':'d':cs) = Card cardId winners gotNos
+  where
+    (cardId, ':':cardBody) = parseNumber cs
+    (winners, remainder) = parseNumberList cardBody
+    (gotNos, _) = parseNumberList remainder
+
+getCardMatches :: Card -> Int
+getCardMatches (Card _ winners gotNos) = sum [1 | n <- gotNos, elem n winners]
+
+getCardPoints :: Card -> Int
+getCardPoints card = Bits.shift 1 $ (getCardMatches card) - 1
+
+-- Boilerplate and solution entrypoints
+
 data Part = One | Two deriving (Show, Ord, Eq, Enum, Bounded)
 instance Read Part where
   readsPrec _ "1" = [(One, "")]
   readsPrec _ "2" = [(Two, "")]
 
 solve :: Part -> String -> String
+solve One = show . sum . (map (getCardPoints . parseCard)) . lines
 solve _ = \_ -> "Unsolved"
 
 main :: IO ()
