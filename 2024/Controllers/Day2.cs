86fe01b3cd348a7f2e8b1806904925631cf1f343
David Monks <david.monks@tradenation.com>
2024-12-03 13:28:30 +0000

    fix(2024): Day 2, Part 2
    This commit simplifies the solution to applying the Part 1 solution
    exactly to each subsequence eliminating one element of each unsafe
    report until a safe subsequence is found (making the report safe) or all
    subsequences have been deemed unsafe (making the report unsafe).
    
    I am still not sure why this works and my previous version doesn't, but
    the difference only seems to surface in one of the sample reports.
    Future work: find this report, and work out what was wrong with my first
    implementation.

--- 2024/Controllers/Day2.cs
+++ 2024/Controllers/Day2.cs
@@ -1,5 +1,4 @@
 using System.Net.Mime;
-using System.Reflection.Emit;
 using _2024.Utilities;
 using Microsoft.AspNetCore.Mvc;
 
@@ -18,18 +17,14 @@ namespace _2024.Controllers
 
         private static readonly string[] newlines = ["\r\n", "\r", "\n"];
 
-        private static int UnsafeAtIndex(int prev, IEnumerable<int> remaining, bool increasing, bool dampnerActive, int index = 1)
+        private static int UnsafeAtIndex(int prev, IEnumerable<int> remaining, bool increasing, int index = 1)
         {
             int current = remaining.FirstOrDefault(-1);
             if (current == -1) return -1;
 
             int difference = increasing ? current - prev : prev - current;
-            if (3 < difference || difference < 1)
-            {
-                if (dampnerActive && UnsafeAtIndex(prev, remaining.Skip(1), increasing, false, index + 1) < 0) return -1;
-                else return index;
-            }
-            return UnsafeAtIndex(current, remaining.Skip(1), increasing, dampnerActive, index + 1);
+            if (3 < difference || difference < 1) return index;
+            return UnsafeAtIndex(current, remaining.Skip(1), increasing, index + 1);
         }
 
         private static bool IsSafe(IEnumerable<int> levels, Part part = Part.One)
@@ -37,17 +32,14 @@ namespace _2024.Controllers
             int first = levels.First();
             IEnumerable<int> remaining = levels.Skip(1);
             bool increasing = remaining.First() > first;
-            bool dampnerActive = part == Part.Two;
-            int unsafeAtIndex = UnsafeAtIndex(first, remaining, increasing, dampnerActive);
+            int unsafeAtIndex = UnsafeAtIndex(first, remaining, increasing);
 
             if (unsafeAtIndex < 0) return true;
-            if (!dampnerActive) return false;
-            if (unsafeAtIndex < 3)
+            if (part == Part.One) return false;
+
+            for (int i = 0; i < levels.Count(); i++)
             {
-                if (IsSafe(remaining)) return true;
-                remaining = remaining.Skip(1);
-                increasing = remaining.First() > first;
-                return UnsafeAtIndex(first, remaining, increasing, false) < 0;
+                if (IsSafe(levels.Where((_, index) => index != i))) return true;
             }
             return false;
         }
